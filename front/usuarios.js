d217aa9a1aa235e1b097fbcf5c7ce9c4e6fa90e7
aragalucas <126295197+aragalucas@users.noreply.github.com>
2025-05-05 11:23:29 -0300

    Add files via upload

---
+++ front/usuarios.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', carregarUsuarios)
+
+function carregarUsuarios() {
+    fetch('http://localhost:8080/usuarios')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erro ao buscar usuários");
+        }
+        return response.json()
+    })
+    .then(usuarios => {
+        const lista = document.getElementById('listaUsuarios')
+        lista.innerHTML = ''
+
+        if (usuarios.lenght === 0) {
+            lista.innerHTML = '<li>Nenhum usuário encontrado. </li>'
+            return;
+        }
+
+        usuarios.forEach(usuario => {
+            const item = document.createElement('li')
+            item.innerHTML = `<strong>Nome:</strong> ${usuario.nome}<br>
+                            <strong>E-mail:</strong> ${usuario.email}<br>
+                            <strong>telefone:</strong> ${usuario.telefone}<br>
+                            <strong>CPF:</strong> ${usuario.cpf}<br>
+                            <strong>CEP:</strong> ${usuario.cep}`
+                            
+            lista.appendChild(item)
+        })
+    })
+    .catch( erro => {
+        document.getElementById('listaUsuarios').innerHTML = 
+        '<li>Erro ao carregar usuários.</li>'
+    })
+}
